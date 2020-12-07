ec5a56918e1aef809a936f0cdd3c15f646f9b53a
heidnick <43583010+heidnick@users.noreply.github.com>
2020-12-07 13:56:08 -0600

    working gets but brackets, push is a maybe

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let path = require('path');
 // NPM modules
 let express = require('express');
 let sqlite3 = require('sqlite3');
+const { resolve } = require('path');
 
 
 let app = express();
@@ -30,33 +31,55 @@ app.use(express.static(public_dir));
 // REST API: GET /codes
 // Respond with list of codes and their corresponding incident type
 app.get('/codes', (req, res) => {
+    console.log('/codes')
     let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
-    
-    res.status(200).type('json').send({});
+    Promise.all([databaseSelect('SELECT * FROM Codes;', [])]).then((results) => {
+        var obj = JSON.parse(JSON.stringify(results));
+        res.status(200).type('json').send(obj);
+    }).catch((error) => {
+        console.log(error);
+    });
 });
 
 // REST API: GET /neighborhoods
 // Respond with list of neighborhood ids and their corresponding neighborhood name
 app.get('/neighborhoods', (req, res) => {
     let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
-
-    res.status(200).type('json').send({});
+    Promise.all([databaseSelect('SELECT * FROM Neighborhoods;', [])]).then((results) => {
+        var obj = JSON.parse(JSON.stringify(results));
+        res.status(200).type('json').send(obj);
+    }).catch((error) => {
+        console.log(error);
+    });
 });
 
 // REST API: GET/incidents
 // Respond with list of crime incidents
 app.get('/incidents', (req, res) => {
     let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
-
-    res.status(200).type('json').send({});
+    Promise.all([databaseSelect('SELECT * FROM Incidents;', [])]).then((results) => {
+        var obj = JSON.parse(JSON.stringify(results));
+        res.status(200).type('json').send(obj);
+    }).catch((error) => {
+        console.log(error);
+    });
 });
 
 // REST API: PUT /new-incident
 // Respond with 'success' or 'error'
 app.put('/new-incident', (req, res) => {
     let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
-
-    res.status(200).type('txt').send('success');
+    /*var test = {
+        case_number: 14171569,
+        date_time: 
+    }*/
+    let query =  `INSERT INTO Incidents (case_number, date_time, code, incident, police_grid, neighborhood_number, block) 
+                  VALUES ($case_number, $date_time, $code, $incident, $police_grid, $neighborhood_number, $block)`;
+    Promise.all([databaseInsert(query, url.searchParams)]).then((results) => {
+        res.status(200).type('txt').send('success');
+    }).catch((error) => {
+        console.log(error);
+    });
 });
 
 
@@ -69,6 +92,7 @@ function databaseSelect(query, params) {
             }
             else {
                 resolve(rows);
+                
             }
         })
     })
